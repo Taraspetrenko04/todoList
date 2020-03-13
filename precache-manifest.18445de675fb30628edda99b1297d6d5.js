0fec6e7b919fa9ba30a858930e49d713855e2de0
Taraspetrenko04 <Taraspetrenko04@gmail.com>
2020-03-13 18:08:03 +0200

    Updates

---
+++ precache-manifest.18445de675fb30628edda99b1297d6d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "477e4c0ed8ca6c10e6b3c1805db34347",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "2ac7c96c6e1232f09e0c",
+    "url": "/todoList/static/css/main.3fb8b239.chunk.css"
+  },
+  {
+    "revision": "d942c63b472118cc77f8",
+    "url": "/todoList/static/js/2.b948560a.chunk.js"
+  },
+  {
+    "revision": "2ac7c96c6e1232f09e0c",
+    "url": "/todoList/static/js/main.86ac9392.chunk.js"
+  },
+  {
+    "revision": "9d24f713e9c32205f14f",
+    "url": "/todoList/static/js/runtime-main.9f34a661.js"
+  }
+]);
